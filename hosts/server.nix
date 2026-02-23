7a05e88acc9efc614e4ea31f92362dba29cd7a98
Shenmarukai <shanemulc@comcast.net>
2026-02-22 21:00:27 -0500

    Fix typo.

--- hosts/server.nix
+++ hosts/server.nix
@@ -107,7 +107,7 @@
     tokenFile = config.sops.secrets.k3s_token.path;
     extraFlags = "--node-ip=10.0.0.1 --bind-address=0.0.0.0 --advertise-address=10.0.0.1 --flannel-iface=eth-direct --tls-san=shane-server.local --tls-san=shane-server";
     manifests = {
-      minecraft = { contents = import ../manifests/minecraft.nix; };
+      minecraft = { content = import ../manifests/minecraft.nix; };
     };
   };
 
