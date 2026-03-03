9411e63a52f6daf990aa66034f9a86e06127576b
Shenmarukai <shanemulc@comcast.net>
2026-03-03 00:50:19 -0500

    Add laptop as trusted ssh and sops device.

--- modules/users/kubernetes.nix
+++ modules/users/kubernetes.nix
@@ -3,11 +3,10 @@
   users.users.kubernetes = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    packages = with pkgs; [
-      tree
-    ];
+
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcqdl2uc402XNn5UMuJIShs74HKiucbwMRpNQT90jJO shane-desktop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA8AA72fp9pvp7RB5JGOhg4tvspcrninYBB/e+e7aCnX shane-laptop"
     ];
   };
 }
